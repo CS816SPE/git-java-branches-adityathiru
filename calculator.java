5481dc3ef4d4e651ace7f92303fe02a0692c215f
Aditya Thiruvengadam <T.Aditya@iiitb.org>
2019-01-29 17:46:54 +0530

    Added calculator.java

---
+++ calculator.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class calculator {
+	public static void main(String args[]){
+		float a, b,res;
+		char choice, ch;
+		Scanner scan  = new Scanner(System.in);
+
+		do{
+
+			System.out.print("1. ADD TWO Numbers\n");
+			System.out.print("2. SUBTRACT TWO Numbers\n");
+			System.out.print("3. Exit\n\n");
+			System.out.print("Enter Your choice : ");
+			choice = scan.next().charAt(0);
+			switch(choice) {
+
+				case '1' : System.out.print("Enter Two numbers : ");
+							a = scan.nextFloat();
+							b = scan.nextFloat();
+							res = a + b;
+							System.out.print("Result = "+res);
+							break;
+
+				case '2' : System.out.print("Enter Two numbers : ");
+							a = scan.nextFloat();
+							b = scan.nextFloat();
+							res = a - b;
+							System.out.print("Result = "+res);
+							break;
+
+				case '3' : System.exit(0);
+							break;
+
+				default : System.out.print("INVALID CHOICE!!!");
+							break;
+
+
+			}
+
+			System.out.print("\n-----------------------\n");
+		}while(choice!=2);
+
+	}
+
+
+}
